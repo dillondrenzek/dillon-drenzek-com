02832c66af98f6e5a53ca7bfff4463fd7729514c
Dillon Drenzek <dillon@drenzek.com>
2026-02-06 22:42:21 -0800

    add main.css

--- server.js
+++ server.js
@@ -15,8 +15,6 @@ app.set('views', path.join(__dirname, '/src/views'));
 app.set('view engine', 'pug');
 
 app.use('/static', express.static(__dirname + '/static'));
-app.use('/vendor', express.static(__dirname + '/vendor'));
-app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
 
 // GET '/resume'
